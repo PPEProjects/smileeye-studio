dafa51dce0b6fa4473e5af12aa0d2db2564ceada
tien <tien.codeby@gmail.com>
2022-06-22 08:59:28 +0700

    apollo goal query

--- src/apollo/smileeye/queries/goal.query.ts
+++ src/apollo/smileeye/queries/goal.query.ts
@@ -16,3 +16,37 @@ export const GOAL_TREE = gql`
     my_tree_goal_v2(goal_id: $goalId)
   }
 `
+
+export const LIST_GOAL_ROOT = gql`
+  query List_goal_root {
+    list_goal_root {
+      id
+      name
+      user_id
+      user {
+        id
+        name
+      }
+      sellRequest {
+        id
+      }
+      description
+      goal_score {
+        id
+      }
+      total_score
+      price
+      status
+      video
+      image
+      type
+      report_type
+      owner_percent
+      detailed_income
+      trial_blocks
+      is_trial
+      start_day
+      created_at
+    }
+  }
+`
